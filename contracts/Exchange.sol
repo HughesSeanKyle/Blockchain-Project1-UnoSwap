261a75db320d375e9454f2932f0ec05797ca480e
HughesSeanKyle <khughessean@yahoo.com>
2021-08-14 18:13:53 +0200

    Create function for adding liquidity

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: MIT
-// 1. Create a token with Token contract 
-// 2. Initialize that contract address with Exchange constructor
-    // List new token on exchanges
-// 3. Make sure address exists before deploying to exchange
+
 contract Exchange {
   address public tokenAddress;
 
@@ -11,4 +8,20 @@ contract Exchange {
 
     tokenAddress = _token;
   }
+
+  function addLiquidity(uint256 _tokenAmount) public payable {
+    IERC20 token = IERC20(tokenAddress);
+    token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
 }
+
+// 1. Create a token with Token contract 
+// 2. Initialize that contract address with Exchange constructor
+    // List new token on exchanges
+// 3. Make sure address exists before deploying to exchange
+// 4. Creating function for adding liquidity 
+    // Make ERC token comply with ERC standards by passing through IERC20 interface (Token (contract) will not deploy otherwise)
+        // Resource - [https://ethereum.stackexchange.com/questions/60940/what-is-ierc20]
+    // Use transferFrom function from IERC20 Interface to 
+        // Transfer token to this exchange contract address and 
+            // allocate the amount
