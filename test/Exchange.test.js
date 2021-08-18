967ad0ef933ae682c22a3f9e8f6d6c4742f196b5
HughesSeanKyle <khughessean@yahoo.com>
2021-08-18 15:54:28 +0200

    Testing different web3 commands

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -14,9 +14,20 @@ contract('Exchange', async (accounts) => {
     
     let supplyInWei = web3.utils.toWei(process.env.INITIAL_TOKENS, 'ether');
     let newToken;
-    let exchange; 
+    let exchange;
+
+    // Helpers 
+    const toWei = (unitInEth) => {
+        return web3.utils.toWei(`${unitInEth}`, 'ether');
+    };
+    
+    const fromWei = (unitInWei) => {
+        return web3.utils.fromWei(`${unitInWei}`, "ether" )
+    };
+
+
     beforeEach(async () => {
-        newToken = await Token.new('TestToken', 'T1', process.env.INITIAL_TOKENS);
+        newToken = await Token.new('TestToken', 'T1', supplyInWei);
         exchange = await Exchange.new(newToken.address);
     });
 
@@ -25,9 +36,40 @@ contract('Exchange', async (accounts) => {
             exchange.address
         ).to.exist; // not strictly (===) equal to either null or undefined
     });
+
+    describe("addLiquidity function", async () => {
+        
+        it("adds liquidity", async () => {
+            let instance = newToken;
+            await instance.approve(exchange.address, toWei(180));
+            let exchangeBal = await web3.eth.getBalance(exchange.address);
+            // let exchangeBalConv = fromWei(exchangeBal.toNumber());
+            
+            let initialHolderBalBefore = await web3.eth.getBalance(initialHolder);
+
+            await exchange.addLiquidity(toWei(180), { value: toWei(90) });
+
+            let initialHolderBalAfter = await web3.eth.getBalance(initialHolder);
+            
+            console.log(exchangeBal);
+            console.log(initialHolderBalBefore, initialHolderBalAfter);
+
+    
+            // expect(await getBalance(exchange.address)).to.equal(toWei(100));
+            // expect(await exchange.getReserve()).to.equal(toWei(200));
+        });
+    
+      });
 });
 
 /*
 To this this file specifically 
 - - test ./test/Exchange.test.js
+
+- - To fix below error just restart truffle console then run test again 
+    - - Error: Returned error: sender doesn't have enough funds to send tx. The upfront cost is: 90134439500000000000 and the sender's account only has: 8692480260000000000
+    - - OR
+    - - Reset the workspace in Ganache
+        - - Very tedious...
+
 */
