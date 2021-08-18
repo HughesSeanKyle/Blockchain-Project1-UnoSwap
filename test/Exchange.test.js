e12b89f46d6df75f5b7c9fa70fc63c6af54746c0
HughesSeanKyle <khughessean@yahoo.com>
2021-08-18 09:46:22 +0200

    Test to see if contract was deployed

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,4 +1,5 @@
 const Token = artifacts.require('Token');
+const Exchange = artifacts.require('Exchange');
 const chai = require('./setupchai');
 
 const BN = web3.utils.BN;
@@ -10,15 +11,23 @@ contract('Exchange', async (accounts) => {
     // First of 3 accounts provided by truffle 
     const [ initialHolder, recipient, anotherAccount ] = accounts;
 
-    let supplyBeforeConvert = process.env.INITIAL_TOKENS;
+    
     let supplyInWei = web3.utils.toWei(process.env.INITIAL_TOKENS, 'ether');
     let newToken;
     let exchange; 
     beforeEach(async () => {
         newToken = await Token.new('TestToken', 'T1', process.env.INITIAL_TOKENS);
+        exchange = await Exchange.new(newToken.address);
     });
 
-    it("Logs data", async () => {
-        console.log(supplyBeforeConvert, supplyInWei);
+    it("Should check if contract was deployed", async () => {
+        return expect(
+            exchange.address
+        ).to.exist; // not strictly (===) equal to either null or undefined
     });
 });
+
+/*
+To this this file specifically 
+- - test ./test/Exchange.test.js
+*/
